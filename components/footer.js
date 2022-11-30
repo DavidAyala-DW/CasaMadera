5a7ed89ecb59a7b97781227f11419807cb1918e3
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2022-11-30 07:40:04 -0500

    show always first image in gallery section in single location page and add mix-blend-exclusion in noble loho

--- components/footer.js
+++ components/footer.js
@@ -196,7 +196,7 @@ export default function Footer(props) {
             {
               footer_noble_link && (
                 <Link passHref href={footer_noble_link} >
-                  <a className="block w-[145px] !mb-[59px] md:!mb-0 vw:w-[7.55vw]">
+                  <a className="block w-[145px] !mb-[59px] md:!mb-0 vw:w-[7.55vw] mix-blend-exclusion">
                     <Image
                       src="/images/noble.svg"
                       alt="Next"
