0d3eabdf10f9f395520fe2cdd80b249b02ca6b27
Austen Morgan <austen@boundaryla.com>
2023-10-18 13:46:51 -0700

    Remove sitemap code

--- next.config.js
+++ next.config.js
@@ -2,18 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/sitemap.xml',
-  //       destination: '/api/sitemap',
-  //     },
-  //     {
-  //       source: '/robots.txt',
-  //       destination: '/api/robots',
-  //     },
-  //   ]
-  // },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
